ae517889c56ebeca676273dcd8ec0d08e5c01748
wanghaoran131 <wanghaoran131@gmail.com>
2023-06-10 22:59:13 +0200

    eva

--- fsdet/evaluation/children_books_evaluation.py
+++ fsdet/evaluation/children_books_evaluation.py
@@ -159,7 +159,7 @@ from pycocotools.cocoeval import COCOeval
 from tabulate import tabulate
 
 
-class COCOEvaluator(DatasetEvaluator):
+class CHILDRENBOOKSEvaluator(DatasetEvaluator):
     """
     Evaluate instance detection outputs using COCO's metrics and APIs.
     """
@@ -199,12 +199,13 @@ class COCOEvaluator(DatasetEvaluator):
             or "novel" in dataset_name
         )
         # fmt: off
-        self._base_classes = [
-            8, 10, 11, 13, 14, 15, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35,
-            36, 37, 38, 39, 40, 41, 42, 43, 46, 47, 48, 49, 50, 51, 52, 53, 54,
-            55, 56, 57, 58, 59, 60, 61, 65, 70, 73, 74, 75, 76, 77, 78, 79, 80,
-            81, 82, 84, 85, 86, 87, 88, 89, 90,
-        ]
+        self._base_classes = self._metadata.base_classes
+        # self._base_classes = [
+        #     8, 10, 11, 13, 14, 15, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35,
+        #     36, 37, 38, 39, 40, 41, 42, 43, 46, 47, 48, 49, 50, 51, 52, 53, 54,
+        #     55, 56, 57, 58, 59, 60, 61, 65, 70, 73, 74, 75, 76, 77, 78, 79, 80,
+        #     81, 82, 84, 85, 86, 87, 88, 89, 90,
+        # ]
         # fmt: on
 
         json_file = PathManager.get_local_path(self._metadata.json_file)
